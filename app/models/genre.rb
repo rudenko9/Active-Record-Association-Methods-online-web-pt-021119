9bc370ed6b0d98349c5032fc70e10b50abe3b67f
Nataliya R <n.rudenko@gmail.com>
2019-05-10 02:25:14 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     artists = []
     self.artists.all.each do |a|
-      artist << a.name 
+      artists << a.name 
     # return an array of strings containing every musician's name
   end
   artists
